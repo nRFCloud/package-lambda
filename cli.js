7cf53986dc3779cd4ff6ec5c32dabab50c36e3ad
Markus Tacker <m@coderbyheart.com>
2019-06-25 09:07:29 +0200

    style: fix errors
    Using npx standard --fix

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander')
-const {local} = require('./index');
+const { local } = require('./index')
 
 program
   .command('local <bucket>')
   .option('-s, --source <directory>', 'Source location', process.env.PWD)
-  .action(async (bucket, {source}) => {
+  .action(async (bucket, { source }) => {
     process.stdout.write(await local(bucket, source))
   })
 
