2335bc6bfb5ce20b0a9e47b85d95dcf385aa25d9
Markus Tacker <m@coderbyheart.com>
2019-06-25 09:07:29 +0200

    style: fix errors
    Using npx standard --fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const {spawn} = require('child_process')
-const {readFile: readFileAsync} = require('fs')
-const {promisify} = require('util')
+const { spawn } = require('child_process')
+const { readFile: readFileAsync } = require('fs')
+const { promisify } = require('util')
 const readFile = promisify(readFileAsync)
 const tmp = require('tmp')
 const path = require('path')
 const ncpAsync = require('ncp').ncp
 const ncp = promisify(ncpAsync)
 const chalk = require('chalk')
-const {S3} = require('aws-sdk')
+const { S3 } = require('aws-sdk')
 
 const run = (cmd, args, options) => new Promise((resolve, reject) => {
   console.error(`${chalk.grey('running:')} ${chalk.yellow(cmd)} ${chalk.yellow(args.join(' '))} ${chalk.grey('...')}`)
@@ -27,7 +27,7 @@ const run = (cmd, args, options) => new Promise((resolve, reject) => {
 
   p.on('close', code => {
     if (code !== 0) return reject(new Error(`${cmd} exited with code ${code}.\n${stderr.join('\n')}`))
-    return resolve({code, stdout: stdout.join('\n'), stderr: stderr.join('\n')})
+    return resolve({ code, stdout: stdout.join('\n'), stderr: stderr.join('\n') })
   })
 })
 
@@ -37,15 +37,15 @@ const createTempDir = () => new Promise((resolve, reject) => {
       if (err) return reject(err)
       console.error(`${chalk.gray('Temp dir:')} ${chalk.yellow(tempDir)}`)
       resolve(tempDir)
-    }, {unsafeCleanup: false})
+    }, { unsafeCleanup: false })
 })
 
 const publishToS3 = async (zipFileName, tempDir, bucket) => {
   const zipFile = `${tempDir}/${zipFileName}`
-  await run('zip', ['-r', '-q', `${zipFile}`, './'], {cwd: tempDir})
+  await run('zip', ['-r', '-q', `${zipFile}`, './'], { cwd: tempDir })
   const zipData = await readFile(zipFile)
   const s3 = new S3()
-  return s3.putObject({Body: zipData, Bucket: bucket, Key: zipFileName}).promise()
+  return s3.putObject({ Body: zipData, Bucket: bucket, Key: zipFileName }).promise()
 }
 
 const existsOnS3 = async (zipFileName, bucket) => {
@@ -62,7 +62,7 @@ const existsOnS3 = async (zipFileName, bucket) => {
 const local = async (bucket, sourcefolder) => createTempDir()
   .then(async tempDir => {
     const pkg = path.join(sourcefolder, 'package.json')
-    const {name, version} = JSON.parse(await readFile(pkg), 'utf-8')
+    const { name, version } = JSON.parse(await readFile(pkg), 'utf-8')
     console.error(`${chalk.blue(name)} ${chalk.green(version)}`)
     const zipFileName = `${name.split('/')[1] || name}-${version}.zip`
     if (await existsOnS3(zipFileName, bucket)) {
@@ -73,7 +73,7 @@ const local = async (bucket, sourcefolder) => createTempDir()
       await ncp(pkg, path.join(tempDir, 'package.json'))
       await ncp(path.join(sourcefolder, 'package-lock.json'), path.join(tempDir, 'package-lock.json'))
       await ncp(path.join(sourcefolder, 'dist'), path.join(tempDir, 'dist'))
-      await run('npm', ['ci', '--ignore-scripts', '--only=prod'], {cwd: tempDir})
+      await run('npm', ['ci', '--ignore-scripts', '--only=prod'], { cwd: tempDir })
       console.error(`${chalk.gray('Uploading to S3:')} ${chalk.yellow(bucket)}${chalk.gray('/')}${chalk.yellow(zipFileName)}`)
       await publishToS3(zipFileName, tempDir, bucket)
       return zipFileName
@@ -82,4 +82,4 @@ const local = async (bucket, sourcefolder) => createTempDir()
     }
   })
 
-module.exports = {local};
+module.exports = { local }
