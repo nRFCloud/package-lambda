853f8beebeeb4ab93e0557f9bfd2b44361fbd342
Cole Hafner <colehafner@gmail.com>
2019-06-24 23:08:12 -0700

    feat: add export so script can be used outside of cli

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const program = require('commander')
+const {local} = require('./index');
+
+program
+  .command('local <bucket>')
+  .option('-s, --source <directory>', 'Source location', process.env.PWD)
+  .action(async (bucket, {source}) => {
+    process.stdout.write(await local(bucket, source))
+  })
+
+program.parse(process.argv)
