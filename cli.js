a7df17ea3282aa8526329eb32e73a653f4e69280
Lincoln Thomas <lincoln.thomas@nordicsemi.no>
2020-02-25 15:52:57 -0800

    feat: various fixes and improvements (#17)
    * feat: iris-1661 add option to install certain packages using their package.json scripts
    * feat: added option to always upload to S3
    * fix: npm output going to stdout broke elivagar buildspec-set-env-vars.sh

--- cli.js
+++ cli.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
+'use strict'
+
 const program = require('commander')
 const { local } = require('./index')
+
+function commaSeparatedList(value, dummyPrevious) {
+  return value.split(',');
+}
 
 program
   .command('local <bucket>')
   .option('-s, --source <directory>', 'Source location', process.env.PWD)
-  .option('-l, --layer', 'Source tree is a Lambda layer. Package the tree as-is. Do not install it as an npm package. A package.json is required but only for the name and version.')
-  .action(async (bucket, { source, layer }) => {
-    process.stdout.write(await local(bucket, source, layer))
+  .option('-l, --layer', 'Source tree is a Lambda layer. Package the tree as-is. ' +
+    'Do not install it as an npm package. A package.json is required but only for the name and version.')
+  .option('--script-install <packages>', 'Run "npm install --only=prod" on the comma-delimited list of ' +
+    'packages, which runs scripts in their package.json. All scripts in all other packages are ignored.', commaSeparatedList)
+  .option('--always-upload', 'Always upload files even if they already exist on S3.')
+  .action(async (bucket, { source, layer, scriptInstall, alwaysUpload }) => {
+    process.stdout.write(await local(bucket, source, layer, scriptInstall, alwaysUpload))
   })
 
 program.parse(process.argv)
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { spawn } = require('child_process')
 const { readFile: readFileAsync } = require('fs')
 const { promisify } = require('util')
@@ -16,7 +18,9 @@ const run = (cmd, args, options) => new Promise((resolve, reject) => {
   let stderr = []
 
   p.stdout.on('data', data => {
-    process.stdout.write(chalk.magenta(data.toString()))
+    // Re-route stdout to stderr, because the only thing the 'local' function should return to stdout
+    // is the name of the zip file.
+    process.stderr.write(chalk.magenta(data.toString()))
     stdout.push(data.toString())
   })
 
@@ -59,27 +63,39 @@ const existsOnS3 = async (zipFileName, bucket) => {
     .catch(() => false)
 }
 
-const local = async (bucket, sourcefolder, layer) => createTempDir()
+const local = async (bucket, sourcefolder, layer, scriptInstallPackages, alwaysUpload) => createTempDir()
   .then(async tempDir => {
     const pkg = path.join(sourcefolder, 'package.json')
     const { name, version } = JSON.parse(await readFile(pkg), 'utf-8')
     console.error(`${chalk.blue(name)} ${chalk.green(version)}`)
     const zipFileName = `${name.split('/')[1] || name}-${version}.zip`
-    if (await existsOnS3(zipFileName, bucket)) {
+    if (!alwaysUpload && await existsOnS3(zipFileName, bucket)) {
       console.error(chalk.yellow(`s3://${bucket}/${zipFileName} exists`))
       return zipFileName
     }
     try {
       if (layer) {
         // For a Lambda layer, zip up the entire source tree. Don't install it.
+        console.error(`${chalk.gray('Packaging as Lambda layer')}`)
         await ncp(sourcefolder, tempDir)
       } else {
         // For a Lambda function, zip up the file structure we expect
+        console.error(`${chalk.gray('Packaging as Lambda function')}`)
         await ncp(pkg, path.join(tempDir, 'package.json'))
         await ncp(path.join(sourcefolder, 'package-lock.json'), path.join(tempDir, 'package-lock.json'))
         await ncp(path.join(sourcefolder, 'dist'), path.join(tempDir, 'dist'))
         // Install the package
         await run('npm', ['ci', '--ignore-scripts', '--only=prod'], { cwd: tempDir })
+      }
+      // For each package in this list, run 'npm install' to run certain scripts defined in its package.json,
+      // as described in https://docs.npmjs.com/misc/scripts
+      // Normally scripts aren't run, due to the --ignore-scripts option in the above "npm ci" command.
+      // This gives the option to run install scripts required for some packages to work, such as 'sharp'.
+      if (scriptInstallPackages) {
+        for (let i = 0; i < scriptInstallPackages.length; i++) {
+          console.error(`${chalk.gray('Installing with scripts:')} ${chalk.yellow(scriptInstallPackages[i])}`)
+          await run('npm', ['install', '--only=prod', scriptInstallPackages[i]], { cwd: tempDir })
+        }
       }
       console.error(`${chalk.gray('Uploading to S3:')} ${chalk.yellow(bucket)}${chalk.gray('/')}${chalk.yellow(zipFileName)}`)
       await publishToS3(zipFileName, tempDir, bucket)
